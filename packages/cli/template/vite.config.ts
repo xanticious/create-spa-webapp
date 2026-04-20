77da1070468b2c57b3113b9547f65574e919e40e
xanticious <TonyLitchfield@Hotmail.com>
2026-04-19 23:09:07 -0600

    Fixes from using scaffolded project.

--- packages/cli/template/vite.config.ts
+++ packages/cli/template/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // relative paths for GitHub Pages deployment
+  base: "/__FOLDER_NAME__/",
   server: {
-    port: parseInt('__PORT__', 10) || 5173,
+    port: parseInt("__PORT__", 10) || 5173,
   },
 });
